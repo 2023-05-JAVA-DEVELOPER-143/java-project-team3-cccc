57f2b32a3660fe0a1767fb73d521dd2f2aa4a2e0
최재용 <133841448+choiyongyong0813@users.noreply.github.com>
2023-07-19 15:34:46 +0900

    Update UserSQL.java

--- Team3_Project/src/user/UserSQL.java
+++ Team3_Project/src/user/UserSQL.java
@@ -4,6 +4,9 @@ public class UserSQL {
 	
 	public static final String USER_INSERT =
 	"insert into userinfo values(?,?,?,?,?,?)";
+
+	public static final String USER_DELETE =
+	"delete from userinfo where userid=?";
 	
 	public static final String USER_FIND_BY_ID =
 	"";
