65fa958906b51c6aa215915a937a06cbec002c09
ITWILL <ykmr0331@naver.com>
2023-07-19 16:41:05 +0900

    PRODUCT 사진

--- Team3_Project/src/product/ProductDao.java
+++ Team3_Project/src/product/ProductDao.java
@@ -1,5 +1,53 @@
 package product;
 
-public class ProductDao {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 
+import common.DataSource;
+
+public class ProductDao {
+	private DataSource dataSource;
+
+	public ProductDao() throws Exception {
+		this.dataSource = new DataSource();
+
+	}
+	
+	public int insert(Product newProduct) throws Exception {
+
+		Connection con = dataSource.getConnection();
+		PreparedStatement pstmt = con.prepareStatement(ProductSQL.PRODUCT_INSERT);
+		pstmt.setString(1, newProduct.getP_name());
+		pstmt.setInt(2, newProduct.getP_price());
+		pstmt.setString(3, newProduct.getP_image());
+		pstmt.setString(4, newProduct.getP_desc());
+		int rowCount = pstmt.executeUpdate();
+		pstmt.close(); 
+		dataSource.close(con);
+
+		return rowCount;
+	}
+	
+/*	public int update(Product updateProduct) throws Exception {
+
+		Connection con = dataSource.getConnection();
+		PreparedStatement pstmt = con.prepareStatement(ProductSQL.PRODUCT_UPDATE);
+//update product set  p_name = ?, p_price = ?, p_image = ?, p_desc = ? where p_no = ?
+		pstmt.setString(1, Pro.getName());
+		pstmt.setString(2, updateAddress.getPhone());
+		pstmt.setString(3, updateAddress.getAddress());
+		pstmt.setInt(4, updateAddress.getNo());
+
+		int rowCount = pstmt.executeUpdate();
+		pstmt.close();
+		dataSource.close(con);
+		return rowCount;
+	}*/
+	
+	
+	
+	
+	
+	
+	
 }
--- Team3_Project/src/product/ProductSQL.java
+++ Team3_Project/src/product/ProductSQL.java
@@ -1,5 +1,22 @@
 package product;
-
+/*
+ ------- -------- ------------- 
+P_NO    NOT NULL NUMBER(10)    
+P_NAME  NOT NULL VARCHAR2(50)  
+P_PRICE NOT NULL NUMBER(10)    
+P_IMAGE NOT NULL VARCHAR2(100) 
+P_DESC           VARCHAR2(200) 
+ */
 public class ProductSQL {
+	
+	
+   public ProductSQL() {
 
+	}
+   
+public static final String PRODUCT_INSERT= "insert into product(p_no,p_name,p_price,p_image,p_desc) values(product_p_no_SEQ.nextval, ?, ?, ?,?)";   
+public static final String PRODUCT_UPDATE= "update product set  p_name = ?, p_price = ?, p_image = ?, p_desc = ? where p_no = ?";    
+public static final String PRODUCT_DELETE = "delete from product where p_no = ?";    
+public static final String Product_SELECT_BY_NO= "select p_no, p_name, p_price, p_image, p_desc from product where p_no = ?";    
+public static final String Product_SELECT_ALL ="select p_no, p_name, p_price, p_image, p_desc from product";
 }
