aaa9607244fc196952cf3df1420eaf743f23c8a3
woosoomi <tnaldn01@gmail.com>
2023-07-19 18:15:49 +0900

    CartDao.java 틀 완성 / CartSQL 약간 수정

--- Team3_Project/src/cart/CartDao.java
+++ Team3_Project/src/cart/CartDao.java
@@ -1,5 +1,7 @@
 package cart;
 
+import java.util.List;
+
 import common.DataSource;
 
 public class CartDao {
@@ -10,19 +12,39 @@ public class CartDao {
 		this.dataSource = new DataSource();
 	}
 	
-	// cart 제품 존재여부
+	// userid cart 내의 제품 존재여부
+	public int countByProductNo(String userid, int p_no) {
+		return 0;
+	}
 	
 	//cart insert
-	
-	//상품에서 카트 추가 시 update
-	
-	//cart List에서 상품 수정
-	
-	//cart List
-	
-	//cart pk delete
-	
-	//cart delete
-	
+	public int insert(Cart cart)  {
+		return 0;
+	}
+	//상품창에서 갯수 추가 update
+	public int updateByProductNo(String userId, int p_no, int cart_qty)  {
+		return 0;
+	}
+	//본인 카트에서 갯수 추가 update
+	public int updateProduct1up(int cart_no, int cart_qty)  {
+		return 0;
+	}
+	//cart List-find
+	public List<Cart> findByUserId(String userId)  {
+		return null;
+	}
+	//cart PK delete
+	public int deleteByCartNo(int cart_no)  {
+		return 0;
+	}
+	//cart userId delete
+	public int deleteByUserId(String userId)  {
+		return 0;
+	}
 	//Cart no로 Cart search
+	public Cart findByCartNo(int Cart_no) {
+	return null;
+	}
+	
+	
 }
