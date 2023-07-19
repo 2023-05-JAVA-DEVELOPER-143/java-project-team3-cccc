002b7ea882abc04d6096cea59f3b09cc1bc49f64
heoseungbum <heoseungbum@naver.com>
2023-07-19 17:28:01 +0900

    ..

--- Team3_Project/src/user/UserService.java
+++ Team3_Project/src/user/UserService.java
@@ -7,16 +7,24 @@ public class UserService {
 		userDao = new UserDao();
 	}
 	
+	public int create(User user) throws Exception{
+		int check = userDao.countByUserId(user.getUserId());
+		if(check < 1) {
+			userDao.insert(user);
+		}else {
+			// 중복
+		}
+		
+		return check;
+	}
+	
 	public User login(String userid,String password)throws Exception{
 		User loginUser =userDao.selectById(userid);
 		if(loginUser!=null && loginUser.getPassword().equals(password)) {
 			
 			return loginUser;
 		}
-		
-		
 		return null;
-			
 	}
 	
 }
