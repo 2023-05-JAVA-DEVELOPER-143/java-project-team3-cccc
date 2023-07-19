8f475743e1067a4cf09e73850f586d75eeec69b6
heoseungbum <heoseungbum@naver.com>
2023-07-19 17:34:34 +0900

    ..

--- Team3_Project/src/user/UserTest.java
+++ Team3_Project/src/user/UserTest.java
@@ -1,6 +1,11 @@
 package user;
 
 public class UserTest {
-	
+	public static void main(String[] args) throws Exception{
+		UserService service = new UserService();	
+		service.create(new User("zzzz", "1234", "zzzz", "제주", "1234", "남"));
+		
+		
+	}
 	
 }
