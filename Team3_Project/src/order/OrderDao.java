d04145bb8dc6ddabf1d941cde498b38f269adeb5
유채린 <cofls000@naver.com>
2023-07-19 16:17:29 +0900

    orderDAO

--- Team3_Project/src/order/OrderDao.java
+++ Team3_Project/src/order/OrderDao.java
@@ -1,5 +1,26 @@
 package order;
 
+import common.DataSource;
+
 public class OrderDao {
+	private DataSource dataSource;
+
+	// 데이터 소스 생성
+	public OrderDao() throws Exception{
+	
+		dataSource = new DataSource();
+	}
+	
+	
+	// 상품주문
+	public void name() {
+		
+	}
+	// 주문개별삭제
+	
+	// 주문전체삭제
+	
+	// 주문리스트보기
+	
 
 }
