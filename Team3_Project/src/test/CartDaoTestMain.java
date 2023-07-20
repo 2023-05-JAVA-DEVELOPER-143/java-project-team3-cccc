d13f0a2436659bc607f4b3c04783ea9794de1e5a
ITWILL <ITWILL@WIN-PDIVQUIHFAF>
2023-07-20 13:40:19 +0900

    ~~

--- Team3_Project/src/test/CartDaoTestMain.java
+++ Team3_Project/src/test/CartDaoTestMain.java
@@ -11,22 +11,26 @@ public class CartDaoTestMain {
 		
 		// userid cart 내의 제품 존재여부
 		
-		int a = cartDao.countByProductNo( "dddd",1 );
-		System.out.println(a);
-		
-		//insert into cart(cart_no,userId,p_no,cart_qty) values(cart_cart_no_seq.nextval, ?,?,?)
-		Product product ;
-		Cart cart = new Cart(0, "aaa", new Product(1, "컴퓨터", 800000, null, "23년도 최신형 컴퓨터"), 2);
-		int c = cartDao.insert(cart);
-		System.out.println(c);
-		
-		// 상품창에서 갯수 추가 update  테스트 완료
-			
-		int b = cartDao.updateByProductNo("dddd", 1, 145);
-		System.out.println(b);
-		
-		
+//		int a = cartDao.countByProductNo( "dddd",1 );
+//		System.out.println(a);
+//		
+//		//insert into cart(cart_no,userId,p_no,cart_qty) values(cart_cart_no_seq.nextval, ?,?,?)
+//		Product product ;
+//		Cart cart = new Cart(0, "aaa", new Product(1, "컴퓨터", 800000, null, "23년도 최신형 컴퓨터"), 2);
+//		int c = cartDao.insert(cart);
+//		System.out.println(c);
+//		
+//		// 상품창에서 갯수 추가 update  테스트 완료
+//			
+//		int b = cartDao.updateByProductNo("dddd", 1, 145);
+//		System.out.println(b);
+//		
+//		//상품 삭제
+//		int d = cartDao.deleteByCartNo(30);
+//		System.out.println(d);
 
+		int e = cartDao.deleteByUserId("aaa");
+		System.out.println(e);
 		
 	}
 
