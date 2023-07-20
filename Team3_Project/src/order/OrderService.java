a42ddf2cf22abb3d3f9789c0d3c2e23f60d0d096
heoseungbum <heoseungbum@naver.com>
2023-07-20 13:30:00 +0900

    sadsad

--- Team3_Project/src/order/OrderService.java
+++ Team3_Project/src/order/OrderService.java
@@ -1,5 +1,7 @@
 package order;
 
+import java.util.List;
+
 import cart.CartDao;
 import product.ProductDao;
 
@@ -19,10 +21,16 @@ public class OrderService {
 	/*
 	 * 주문목록
 	 */
+	public List<Order> OrderList(String userid) throws Exception{
+		return orderDao.findOrderbyUserId(userid);
+	}
 	
 	/*
 	 * 주문상세보기
 	 */
+	public Order OrderDetail(Order order) throws Exception{
+		return orderDao.findOrderWithProductByUserId(order.getUserId(), order.getO_no());
+	}
 	
 	/*
 	 * cart에서 주문
