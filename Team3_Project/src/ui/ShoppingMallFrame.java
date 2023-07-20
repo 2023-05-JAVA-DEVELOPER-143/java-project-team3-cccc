3092d85316893f8e368f8f29ecb970c77b6f778d
woosoomi <tnaldn01@gmail.com>
2023-07-20 15:27:07 +0900

    ..

--- Team3_Project/src/ui/ShoppingMallFrame.java
+++ Team3_Project/src/ui/ShoppingMallFrame.java
@@ -37,7 +37,6 @@ import java.awt.Rectangle;
 import java.awt.GridLayout;
 
 
-
 import cart.CartService;
 import order.OrderService;
 import product.ProductService;
@@ -83,7 +82,6 @@ public class ShoppingMallFrame extends JFrame {
 	private JPanel order_ContentPane;
 	private JScrollPane cart_ScrollPane;
 	private JPanel cart_ContentPanel;
-	private JComboBox info_GenderComboBox;
 
 	/**
 	 * Launch the application.
@@ -555,6 +553,10 @@ public class ShoppingMallFrame extends JFrame {
 		shop_LoginPanel.add(login_PasswordField);
 		
 		JButton login_Btn = new JButton("로그인");
+		login_Btn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
 		login_Btn.setBounds(143, 342, 97, 23);
 		shop_LoginPanel.add(login_Btn);
 		
@@ -667,7 +669,6 @@ public class ShoppingMallFrame extends JFrame {
 		
 		JPanel shop_InfoPanel = new JPanel();
 		shopTabbedPane.addTab("내정보", null, shop_InfoPanel, null);
-		shopTabbedPane.setEnabledAt(3, false);
 		shop_InfoPanel.setLayout(null);
 		
 		JLabel info_IdLabel = new JLabel("아이디");
@@ -727,16 +728,16 @@ public class ShoppingMallFrame extends JFrame {
 		JButton info_Btn = new JButton("회원탈퇴");
 		info_Btn.setFont(new Font("굴림", Font.BOLD, 15));
 		info_Btn.setBackground(Color.WHITE);
-		info_Btn.setBounds(287, 381, 95, 33);
+		info_Btn.setBounds(107, 381, 95, 33);
 		shop_InfoPanel.add(info_Btn);
 		
 		JButton info_Cancle_Btn = new JButton("취소");
 		info_Cancle_Btn.setFont(new Font("굴림", Font.BOLD, 15));
 		info_Cancle_Btn.setBackground(Color.WHITE);
-		info_Cancle_Btn.setBounds(394, 381, 71, 33);
+		info_Cancle_Btn.setBounds(311, 381, 71, 33);
 		shop_InfoPanel.add(info_Cancle_Btn);
 		
-		info_GenderComboBox = new JComboBox();
+		JComboBox info_GenderComboBox = new JComboBox();
 		info_GenderComboBox.setModel(new DefaultComboBoxModel(new String[] {"성별", "남성", "여성"}));
 		info_GenderComboBox.setBounds(178, 317, 92, 23);
 		shop_InfoPanel.add(info_GenderComboBox);
@@ -764,7 +765,7 @@ public class ShoppingMallFrame extends JFrame {
 		});
 		info_Cancle_Btn_1.setFont(new Font("굴림", Font.BOLD, 15));
 		info_Cancle_Btn_1.setBackground(new Color(255, 255, 102));
-		info_Cancle_Btn_1.setBounds(190, 381, 85, 33);
+		info_Cancle_Btn_1.setBounds(214, 381, 85, 33);
 		shop_InfoPanel.add(info_Cancle_Btn_1);
 		
 		JLabel info_Icon = new JLabel("");
@@ -777,24 +778,6 @@ public class ShoppingMallFrame extends JFrame {
 		lblNewLabel.setBounds(224, 54, 129, 39);
 		shop_InfoPanel.add(lblNewLabel);
 		
-
-		JButton info_User_Btn = new JButton("회원정보");
-		info_User_Btn.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				
-				displayUserInfo(loginUser);
-				
-			}
-		});
-		info_User_Btn.setFont(new Font("굴림", Font.BOLD, 15));
-		info_User_Btn.setBackground(Color.WHITE);
-		info_User_Btn.setBounds(83, 381, 95, 33);
-		shop_InfoPanel.add(info_User_Btn);
-		
-		JPanel shop_AdminPanel = new JPanel();
-		shopTabbedPane.addTab("관리창", null, shop_AdminPanel, null);
-		
-
 		JPanel shop_CartPanel = new JPanel();
 		shopTabbedPane.addTab("장바구니", null, shop_CartPanel, null);
 		shop_CartPanel.setLayout(new BorderLayout(0, 0));
@@ -1251,26 +1234,8 @@ public class ShoppingMallFrame extends JFrame {
 		this.loginUser = loginUser;
 		setTitle(loginUser.getName() + "님 로그인");
 		shopTabbedPane.setEnabledAt(1, false);
-
-		shopTabbedPane.setEnabledAt(3, true);
-		shopTabbedPane.setSelectedIndex(0);
-		
-
 		shopTabbedPane.setSelectedIndex(0);
 	}
 
-	private void displayUserInfo(User loginUser) {
-		/****로그인한회원상세데이타보여주기*****/
-		info_IdTextField.setText(loginUser.getUserId());
-		info_AddressTextField.setText(loginUser.getAddress());
-		info_NameTextField.setText(loginUser.getName());
-		info_PasswordField.setText(loginUser.getPassword());
-		info_PhoneTextField.setText(loginUser.getPhone());
-		info_GenderComboBox.setSelectedItem(loginUser.getGender()+"");
-		
-	} // 생성자 끝
-}
 
-
-
-
+		}
