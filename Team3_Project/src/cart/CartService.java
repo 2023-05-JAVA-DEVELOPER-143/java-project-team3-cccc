a802d55d98b2ca506f51dbefc83518c5e9a2fc8a
woosoomi <tnaldn01@gmail.com>
2023-07-20 13:38:52 +0900

    CartService - Delete 파트 완료

--- Team3_Project/src/cart/CartService.java
+++ Team3_Project/src/cart/CartService.java
@@ -1,6 +1,11 @@
 package cart;
 
 public class CartService {
+	private CartDao cartDao;
+	
+	public CartService() throws Exception{
+		this.cartDao = new CartDao();
+	}
 	
 	/*
 	 * 카트추가 or 수정
@@ -21,9 +26,14 @@ public class CartService {
 	/*
 	 * 카트아이템1개삭제
 	 */
+	public int cartDeleteByCartNo(int cart_no) throws Exception {
+		return cartDao.deleteByCartNo(cart_no);
+	}
 	
 	/*
-	 * 카트삭제
+	 * 카트 모두 삭제
 	 */
-	
+	public int cartDeleteByUserId(String userId) throws Exception  {
+		return cartDao.deleteByUserId(userId);
+	}
 }
